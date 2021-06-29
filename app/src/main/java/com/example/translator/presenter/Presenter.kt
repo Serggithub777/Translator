baab84c19026016e9e4d0f98d874102ffef6f911
SergSerg <sergio.ne@inbox.ru>
2021-06-29 12:55:52 +0600

    Добавил интерфейс Presenter

---
+++ app/src/main/java/com/example/translator/presenter/Presenter.kt
@@ -0,0 +1,10 @@
+package com.example.translator.presenter
+
+interface Presenter<T : AppState, V : View> {
+
+    fun attachView(view: V)
+
+    fun detachView(view: V)
+
+    fun getData(word: String, isOnline: Boolean)
+}
