355b3ac1f6a04ff0f7f026a612532073011f0a7e
SergSerg <sergio.ne@inbox.ru>
2021-07-04 16:42:58 +0600

    Добавил MainInteractor.kt

--- app/src/main/java/com/example/translator/view/MainInteractor.kt
+++ app/src/main/java/com/example/translator/view/MainInteractor.kt
@@ -10,16 +10,18 @@ import io.reactivex.Observable
 import javax.inject.Inject
 import javax.inject.Named
 
-class MainInteractor @Inject constructor(
-    @Named(NAME_REMOTE) val repositoryRemote: Repository<List<DataModel>>,
-    @Named(NAME_LOCAL) val repositoryLocal: Repository<List<DataModel>>
+class MainInteractor(
+    private val repositoryRemote: Repository<List<DataModel>>,
+    private val repositoryLocal: Repository<List<DataModel>>
 ) : Interactor<AppState> {
 
-    override fun getData(word: String, fromRemoteSource: Boolean): Observable<AppState> {
-        return if (fromRemoteSource) {
-            repositoryRemote
-        } else {
-            repositoryLocal
-        }.getData(word).map { AppState.Success(it) }
+    override suspend fun getData(word: String, fromRemoteSource: Boolean): AppState {
+        return AppState.Success(
+            if (fromRemoteSource) {
+                repositoryRemote
+            } else {
+                repositoryLocal
+            }.getData(word)
+        )
     }
 }
