f0ca0460175c89acacef7850d7af33cc9b155a6d
SergSerg <sergio.ne@inbox.ru>
2021-07-06 19:09:56 +0600

    Добавил TranslatorApp.kt

--- app/src/main/java/com/example/translator/application/TranslatorApp.kt
+++ app/src/main/java/com/example/translator/application/TranslatorApp.kt
@@ -3,6 +3,7 @@ package com.example.translator.application
 import android.app.Application
 import com.example.translator.dagger.application
 import com.example.translator.dagger.mainScreen
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class TranslatorApp : Application() {
@@ -10,7 +11,8 @@ class TranslatorApp : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            modules(listOf(application, mainScreen))
+            androidContext(applicationContext)
+            modules(listOf(application, mainScreen, historyScreen))
         }
     }
 }
