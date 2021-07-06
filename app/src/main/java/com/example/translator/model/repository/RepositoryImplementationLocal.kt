c618dd777034ab3af3ae540151af6c822f18fc86
SergSerg <sergio.ne@inbox.ru>
2021-07-06 19:18:30 +0600

    Добавил RepositoryImplementationLocal.kt

---
+++ app/src/main/java/com/example/translator/model/repository/RepositoryImplementationLocal.kt
@@ -0,0 +1,13 @@
+package com.example.translator.model.repository
+
+class RepositoryImplementationLocal(private val dataSource: DataSourceLocal<List<DataModel>>) :
+    RepositoryLocal<List<DataModel>> {
+
+    override suspend fun getData(word: String): List<DataModel> {
+        return dataSource.getData(word)
+    }
+
+    override suspend fun saveToDB(appState: AppState) {
+        dataSource.saveToDB(appState)
+    }
+}
