dd28d23ec8a470b8cdcc3207abc33d859dc2644b
SergSerg <sergio.ne@inbox.ru>
2021-07-06 18:54:05 +0600

    Добавил HistoryInteractor.kt

---
+++ app/src/main/java/com/example/translator/view/history/HistoryInteractor.kt
@@ -0,0 +1,17 @@
+package com.example.translator.view.history
+
+class HistoryInteractor(
+    private val repositoryRemote: Repository<List<DataModel>>,
+    private val repositoryLocal: RepositoryLocal<List<DataModel>>
+) : Interactor<AppState> {
+
+    override suspend fun getData(word: String, fromRemoteSource: Boolean): AppState {
+        return AppState.Success(
+            if (fromRemoteSource) {
+                repositoryRemote
+            } else {
+                repositoryLocal
+            }.getData(word)
+        )
+    }
+}
