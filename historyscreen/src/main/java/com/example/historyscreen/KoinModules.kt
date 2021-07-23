278f3c7496fbabf95f8c0fe67392899482da6ad8
SergSerg <sergio.ne@inbox.ru>
2021-07-23 22:35:08 +0600

    Добавил модуль strings.xml

---
+++ historyscreen/src/main/java/com/example/historyscreen/KoinModules.kt
@@ -0,0 +1,17 @@
+package com.example.historyscreen
+
+import com.example.historyscreen.view.HistoryInteractor
+import com.example.historyscreen.view.HistoryViewModel
+import org.koin.core.context.loadKoinModules
+import org.koin.dsl.module
+
+fun injectDependencies() = loadFeature
+
+private val loadFeature by lazy {
+    loadKoinModules(listOf(historyScreen))
+}
+
+val historyScreen = module {
+    factory { HistoryViewModel(get()) }
+    factory { HistoryInteractor(get(), get()) }
+}
